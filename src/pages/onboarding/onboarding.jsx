e53a02b4407d271135b9e997b84c61113db59760
wanweileee <wanweileee@gmail.com>
2025-06-15 03:40:15 +0800

    c

--- src/pages/onboarding/onboarding.jsx
+++ src/pages/onboarding/onboarding.jsx
@@ -6,15 +6,7 @@ export default function Onboarding() {
   return (
     <div className={styles.container}>
       <img src={piggybank} alt="Piggy Bank" className={styles.image} />
-
-      <button className={styles.getStartedBtn}>Get Started</button>
-
-      <p className={styles.loginText}>
-        Already have an account?{' '}
-        <Link to="/login" className={styles.loginLink}>
-          Log in
-        </Link>
-      </p>
+     <Link to="/login" className={styles.getStartedBtn}>Login</Link>
     </div>
   );
 }
